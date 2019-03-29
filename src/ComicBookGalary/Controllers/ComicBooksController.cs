888256c4a77c5dffeff19fe494c65c5d2244ad85
tijmenwritesprograms <tijmenvanderhilst@gmail.com>
2019-03-29 22:22:47 +0100

    Added repository and Index Action and View

--- src/ComicBookGalary/Controllers/ComicBooksController.cs
+++ src/ComicBookGalary/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGalary.Models;
+﻿using ComicBookGalary.Data;
+using ComicBookGalary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,29 @@ namespace ComicBookGalary.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
-        {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Role = "Script", Name = "Dan Slott"},
-                    new Artist() {Role = "Pencils", Name = "Humberto Ramos"},
-                    new Artist() {Role = "Inks", Name = "Victor OLazaba"},
-                    new Artist() {Role = "Colors", Name = "Edgar Delgado"},
-                    new Artist() {Role = "Letters", Name = "Chris Eliopoulos"}
-                }
-            };
+        ComicBookRepository _comicBookRepository = null;
 
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
+           
             return View(comicBook);
         }
     }
